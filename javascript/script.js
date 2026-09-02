3fc60d408d164fa346656ce6da3cc40bb5efe870
KDEL-Dev <lbrk0024@humber.ca>
2026-09-01 22:39:05 -0400

    creating randomizer for teams

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,33 @@
 // ---------------- DRAFT PAGE --------------------- //
 
+// Draft Order - Shuffle
+const draftOrder = document.getElementById("draftOrder");
+const randomizeBtn = document.getElementById("randomizeDraft")
+
+let teams = [
+    "Normal",
+    "Dark",
+    "Bug",
+    "Fairy",
+    "Steel"
+]
+
+randomizeBtn.addEventListener("click", function()
+{
+
+    teams.sort(() => Math.random() -0.5);
+
+    draftOrder.innerHTML = "";
+
+    teams.forEach((team, index) => {
+        const li = document.createElement("li");
+
+        li.textContent = `${index + 1}. ${team}`;
+
+        draftOrder.appendChild(li)
+    });
+});
+
 // select draft buttons by class name
 let draftButtons = document.querySelectorAll(".draftBtn")
 
